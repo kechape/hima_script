d06fce99d34d912bbc10564b3e6daca6b9cc471a
けちゃぺ <129019939+kechape@users.noreply.github.com>
2024-07-15 12:33:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 setInterval(function(){
 document.getElementById("t1").innerHTML = new Date().toLocaleString();
 },500);
+x = 99;
+y = 234;
 
-document.getElementById("bn").innerHTML = navigator.appName;
+
+document.getElementById("bn").innerHTML = x + y;
 
 
