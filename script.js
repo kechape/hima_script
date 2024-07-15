8605eddd1a05a818ca511e0cb69c576ee09b4f28
けちゃぺ <129019939+kechape@users.noreply.github.com>
2024-07-15 12:16:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,4 @@ setInterval(function(){
 document.getElementById("t1").innerHTML = new Date().toLocaleString();
 },500);
 
-document.getElementById("myButton").addEventListener("click", function() {
-  document.getElementById("myDiv").innerHTML = "はたわ";
-});
+
