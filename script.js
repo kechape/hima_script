d506f17f4bc7e178cbf758173cb7efe5d7107eee
けちゃぺ <129019939+kechape@users.noreply.github.com>
2024-08-31 17:04:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.getElementById("t1").innerHTML = new Date().toLocaleString();
 setInterval(function(){
 document.getElementById("num").innerHTML = x;
 },1);
+setInterval(function(){
+ if(x > 10000000){ 
+ document.getElementById("comennt").innerHTML ="これだけの数をたたき出すなんて....まったく";
+ }
+},1);
+
 
 let x = 0;
 
